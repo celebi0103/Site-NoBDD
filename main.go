2cee063d9532d00969df8a38d386c94b18093fe7
ACELEBI <ali.celebi@ynov.com>
2023-04-05 09:38:14 +0200

    Base du site

---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+func main() {
+
+	Handlefunc()
+}
+func Handlefunc() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./index.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+	http.HandleFunc("/maintenance", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("Page/index.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/contact.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+	http.HandleFunc("/equipes", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/equipes.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+	http.HandleFunc("/blog", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/blog.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	http.HandleFunc("/articles", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/articles.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	http.HandleFunc("/articlesB", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/articleBoxe.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	http.HandleFunc("/articlesBasket", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/articlebasket.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	http.HandleFunc("/articlesHandi", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/articlehandi.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	// http.HandleFunc("/website/bienvenu.php", func(w http.ResponseWriter, r *http.Request) {
+	// 	template := template.Must(template.ParseFiles("templates/header.html", "templates/footer.html"))
+	// 	if r.Method != http.MethodPost {
+	// 		template.Execute(w, "")
+	// 		return
+	// 	}
+	// })
+
+	http.HandleFunc("/services", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/nos_services.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	fs := http.FileServer(http.Dir("Static/"))
+	http.Handle("/Static/", http.StripPrefix("/Static/", fs))
+	fmt.Println("http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
